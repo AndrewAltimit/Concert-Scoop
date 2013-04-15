cc7dcb3dd0e474b77a1d0ac0cbe936ccd0d0512e
unknown <andrewshowers09@OlinLab204-14.augustana.edu>
2013-04-15 13:48:02 -0500

    Created a JSONParser which can return JSON data from a URL Some Code(Incomplete) to read the concert list JSON Data (getConcerts)

--- Concert Scoop/src/edu/augustana/concertscoop/models/Concert.java
+++ Concert Scoop/src/edu/augustana/concertscoop/models/Concert.java
@@ -1,43 +1,81 @@
 package edu.augustana.concertscoop.models;
 
 import java.util.ArrayList;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Concert {
 
-	public void Concert(int server_id){
-		//Make a connection with the server
-		
-		
-		//pull data from server about this concert based on the id number
-		//And put the data into an arraylist
+	public void Concert(int server_id) {
+		// Make a connection with the server
+
+		// pull data from server about this concert based on the id number
+		// And put the data into an arraylist
 		ArrayList<String> temp = new ArrayList<String>();
-		//Populate fields with the pulled data
+		// Populate fields with the pulled data
 		Concert(temp);
-		
+
 	}
-	public void Concert(ArrayList<String> properties){
-		//title = properties(0)
-		//date = properties(1)
-		//...
+
+	public void Concert(ArrayList<String> properties) {
+		// title = properties(0)
+		// date = properties(1)
+		// ...
 	}
-	
-	//Return the list of concerts
+
+	// Return the list of concerts
 	public static ArrayList<Concert> getConcerts() {
-		//http request to server for all concerts (or subset)
-		//get back json data
-		//parse json into concert objects
-			//concert t1 = new Concert(Properties Array);
-		//add all concert objects into arraylist
 		
+		//Pseudocode
+		// http request to server for all concerts (or subset)
+		// get back json data
+		// parse json into concert objects
+		// concert t1 = new Concert(Properties Array);
+		// add all concert objects into arraylist
+
+		// Creating JSON Parser instance
+		JSONParser jParser = new JSONParser();
+
+		// getting JSON string from URL
+		JSONObject json = jParser.getJSONFromUrl(url);
+		
+		//DEBUGGING Show contents of JSON Data
+		System.out.print(json.toString());
+
+		//Go through all the elements in the JSON String
+		try {
+		    // Getting Array of Concerts
+			JSONArray concerts;
+			concerts = json.getJSONArray("TAG_CONCERTS");
+
+		    // looping through All Concerts
+		    for(int i = 0; i < concerts.length(); i++){
+		        JSONObject c = concerts.getJSONObject(i);
+		        
+		        //Debugging
+		        System.out.print(c.toString());
+		        
+		        
+		        // Storing each json item in variable
+		        //String title = c.getString("Title");
+
+		    }
+		} catch (JSONException e) {
+		    e.printStackTrace();
+		}
+		
+		//Debugging Purposes - Return empty array
 		ArrayList<Concert> temp = new ArrayList<Concert>();
-		return  temp; 
+		return temp;
 	}
-	
-	//Make a new concert entry
-	public boolean  update(){
+
+	// Make a new concert entry
+	public boolean update() {
 		return true;
 	}
-	
+
 	private String title;
-	
+	static String url = "http://127.0.0.1:3000/concerts.json";
+
 }
